2102c92857db25687d7209cae3c54bcb444a1a85
priya1823 <43338408+priya1823@users.noreply.github.com>
2018-09-18 16:27:15 +0530

    Create file5.c

---
+++ file5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+clrscr();
+printf("\nEnter a letter:");
+scanf("%c",&c);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+{
+printf("\nVowels")
+}
+else
+{
+printf("\nConsonant");
+}
+getch();
+}
