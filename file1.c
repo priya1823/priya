acace9c9f1eea4ba65caf46fb04b2a52ab7ab3ba
priya1823 <43338408+priya1823@users.noreply.github.com>
2018-09-18 14:51:18 +0530

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,22 @@
+#include<stdio.>
+#include<conio.h>
+void main(0
+{
+int a;
+clrscr();
+printf("enter the value:");
+scanf("%d",&a);
+if(a==0)
+{
+printf("\nZERO");
+}
+else if(a<0)
+{
+printf("\nNEGATIVE");
+}
+else if(a>0)
+{
+printf("\nPOSITIVE");
+}
+getch();
+}
