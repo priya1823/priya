5c704c128a2566473c9191419e389c7300066ee5
priya1823 <43338408+priya1823@users.noreply.github.com>
2018-09-18 15:21:55 +0530

    Create file2.c

---
+++ file2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter the number:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\nEven");
+}
+else
+{
+printf("\nOdd");
+}
+getch();
+}
